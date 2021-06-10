31d05ecfc2904327859be2601828e23ce5e66d8d
Hew220 <hew220@gmail.com>
2021-06-10 21:12:01 +0200

    Add initial spring boot project. closes #1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # EmployeeTableApp
+
 EmployeeTableApp
---
+++ src/test/java/guru/springframework/employeemanagementsystemapp/EmployeeManagementSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.employeemanagementsystemapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
